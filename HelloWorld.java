64c8dcd98ff2a7c740eae542fdc4082cf9a400d8
Grace Preston <143225518+Grass-Rat@users.noreply.github.com>
2023-12-19 11:45:05 -0500

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,5 @@
 public class Main {
   public static void main(String[] args) {
-    System.out.print("Hello World! ");
-    System.out.println("This will print on the same line.");
+    System.out.print("Hello World!");
   }
 }
